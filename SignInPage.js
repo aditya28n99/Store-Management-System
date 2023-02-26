96c19cfcf65d642d9b0c90e5a2c0fada395a312e
Aditya Sunil Wakale <adityawakale208@gmail.com>
2023-02-26 18:53:37 +0530

    Sign in complete.. Orders page is assigned

--- SignInPage.js
+++ SignInPage.js
@@ -4,8 +4,9 @@ const userName = document.getElementById("userName");
 const passWord = document.getElementById("passWord");
 
 clickToLogin = ()=>{
-    if(userName.value == passWord.value){
-        alert("Succesfully Logged In");
+    if(userName.value == passWord.value && userName.value !== ''){
+        window.location.assign("OrdersPage.html");
+        alert("Succesfully Logged In");       
     }else{
         alert("Please Enter Valid Credentials");
     }
