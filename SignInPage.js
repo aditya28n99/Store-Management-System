37c01a3776f5f32b08b126b6cc5ab161b5f62710
Aditya Sunil Wakale <adityawakale208@gmail.com>
2023-02-27 22:13:52 +0530

    Project sm commit

--- SignInPage.js
+++ SignInPage.js
@@ -3,7 +3,26 @@
 const userName = document.getElementById("userName");
 const passWord = document.getElementById("passWord");
 
+var arrForUsers = new Array();
+let lsForUser = new Object();
+
+arrForUsers = {
+    "name": userName.value,
+    "password": passWord.value    
+}
+
+console.log(arrForUsers["name"]);
+console.log(arrForUsers["password"]);
+
+
+sessionStorage.setItem("currentloggedin", userName);
+
+localStorage.setItem('all_users',JSON.stringify(arrForUsers));
+
+arrForUsers=JSON.parse((localStorage.getItem("all_users")));
+
 clickToLogin = ()=>{
+    
     if(userName.value == passWord.value && userName.value !== ''){
         window.location.assign("OrdersPage.html");
         alert("Succesfully Logged In");       
